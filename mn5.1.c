b762e388c3241fdc2cf81009f48c56dd38e76c41
Meerahari <meerasudha1997@gmail.com>
2017-04-18 14:34:33 +0530

    Add files via upload

---
+++ mn5.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,sum=0,c;
+printf("enter the number:");
+scanf("%d",&a);
+while(a!=0)
+{
+c=a%10;
+sum=sum+(c*c*c);
+a=a/10;
+}
+if(a==sum)
+{
+printf("the entered number is armstrong number");
+}
+else
+{
+printf("entered number is not a armstrong number:");
+}
+}
