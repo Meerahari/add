c90e49370d6269a8f77114d433522d8b1ba46408
Meerahari <meerasudha1997@gmail.com>
2017-04-18 14:55:02 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&0a);
+if(a<0)
+{
+printf("the number is negative");
+}
+if(a>0)
+{
+printf("the number is positive");
+}
+if(a==0)
+{
+printf("the number is zero");
+}
+}
