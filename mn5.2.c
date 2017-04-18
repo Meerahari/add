8bc31c20f1791db5dbcd7a97ce054142008ce3c5
Meerahari <meerasudha1997@gmail.com>
2017-04-18 14:58:17 +0530

    Add files via upload

---
+++ mn5.2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ int x;
+ printf("enter a number:");
+ scanf("%d",&x);
+ if(x%2==0)
+{
+
+
+ printf("%d is even number.",x);
+
+}
+else{
+ printf("%d is odd number.",x);
+ }
+
+}
